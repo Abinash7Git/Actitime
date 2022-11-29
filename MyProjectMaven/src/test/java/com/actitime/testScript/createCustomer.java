35aa1709105730589f983ae520f24cd0156b714f
Abinash Sahoo <Abinash Sahoo@AbinashHP>
2022-11-29 20:47:40 +0530

    Maven

---
+++ MyProjectMaven/src/test/java/com/actitime/testScript/createCustomer.java
@@ -0,0 +1,14 @@
+package com.actitime.testScript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class createCustomer {
+	
+	@Test
+	public void createCustomer()
+	{
+		Reporter.log("Create Customer",true);
+	}
+
+}
